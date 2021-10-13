c0e06dd2f9487ea01ceaf4717b7c758ee1236e7e
sagor <sagorahamed017997@gmail.com>
2021-10-13 20:00:21 +0600

    Updated freebootcamp.js and main.js

---
+++ freebootcamp.js
@@ -0,0 +1,65 @@
+
+// function frankenSplice(arr1, arr2, n) {
+//     console.log(arr1);
+//     console.log(arr2);
+//     console.log(n);
+
+//   }
+  
+//   frankenSplice([1, 2, 3], [, 5, 6], 1);
+
+
+// function bouncer(arr) {
+//     console.log(arr);
+//     let count = [];
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i]) count.push(arr[i])
+//     }
+//     return count;
+//   }
+  
+//   console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+
+
+
+// function getIndexToIns(arr, num) {
+//     console.log(arr)
+//     console.log(num);
+//     let count = 0;
+//     for(let val of arr){
+//         if(val < num){
+//             count++;
+//         }
+//     }
+//     return count;
+//   }
+  
+//   console.log(getIndexToIns([40,30,10, 60], 50));
+
+
+
+
+// function mutation(arr) {
+//     return arr[1]
+//     .toLowerCase()
+//     .split('')
+//     .every((letter) => {
+//         return arr[0].toLowerCase().indexOf(letter) != -1;
+//     })
+//   }
+  
+//   console.log(mutation(["hello", "hey"]));
+
+
+
+
+function chunkArrayInGroups(arr, size) {
+    let sum = [];
+    sum.push(arr.length/size)
+    console.log(sum)
+
+  }
+  
+  chunkArrayInGroups(["a", "b", "c", "d","b"], 2);
+
+
---
+++ main.js
@@ -0,0 +1,68 @@
+// // const h1 = document.querySelector(".heading");
+// // function eventInfo(evt){
+// //     console.log(evt);
+// //     console.log("H1 is clicked");
+// //     console.log("Type", evt.type);
+// //     console.log("Target", evt.target);
+// //     console.log("offset-x", evt.offsetX);
+// //     console.log("offset-y", evt.offsetY);
+// //     console.log("client-x", evt.clientX);
+// //     console.log("client-y", evt.clientY);
+// // };
+
+// // h1.addEventListener('click', eventInfo)
+
+// // const li = document.querySelector('.mainLi');
+// // const ul = document.querySelector('.mainUl');
+// // const container = document.querySelector('.container');
+// // li.addEventListener('click', (evt) =>{
+// //     console.log(evt.target)
+// // })
+// // ul.addEventListener('click', (evt) =>{
+// //     console.log("you clicked li")
+// // })
+// // container.addEventListener('click', (evt) =>{
+// //     console.log("you clicked li")
+// // })
+// const form = document.forms[0];
+// const input = document.querySelector(".input");
+// const submit = document.querySelector(".submit");
+// const li = document.querySelector(".mainLi");
+// const ul = document.querySelector(".mainUl");
+
+// function ieventInfo(evt){
+//     evt.preventDefault();
+//     const li = document.createElement('li');
+//     li.textContent = input.value;
+//     li.classList.add = 'mainLi';
+//     ul.appendChild(li);
+// }
+
+// form.addEventListener('submit', ieventInfo);
+
+
+
+// // function booWho(bool) {
+// //     if(bool === true || bool === false || bool === "" || bool === 0 || bool === null || bool === undefined){
+// //         return true
+// //     }else{
+// //         return false;
+// //     }
+// //   }
+  
+// //   console.log(booWho("null"));
+
+// // function titleCase(str) {
+// //     console.log(str);
+
+// //     const arr = str.toLowerCase().split(' ');
+// //     const result = arr.map(function(val){
+// //         return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+// //     })
+// //     return result.join(" ")
+// //   }
+  
+// //   console.log(titleCase("I'm a little tea pot"));
+
+
+
